f8f87721886faa0b75bff492e603319533ce2395
StevenMJMarshall <stevenmarshall@mdr.net>
2015-06-16 08:28:39 -0500

    Getting ready for angular use with google map api.

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-angular
-	.module("MainApp", function() {
-		
+angular.module("map")
+	.controller("MainCtrl", function ($scope, $http) {
+		$http.get('https://data.sparkfun.com/output/wpvj38xXg8HbE4x9ndYN.json')
+		.then(function(Data) )
+		$scope.map = {
+			center: {
+
+			}
+		}
 	})
